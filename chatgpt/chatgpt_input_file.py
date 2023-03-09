a976780287b0c4da235af63bc1d92d021aa22eee
pikaic <pikaic2010@gmail.com>
2023-03-09 04:44:43 -0800

    added changes

--- chatgpt/chatgpt_input_file.py
+++ chatgpt/chatgpt_input_file.py
@@ -1,13 +1,20 @@
 #! /home/logan_18/anaconda3/envs/pepperenv/bin/python3
 # -*- encoding: UTF-8 -*-
 
-
+import whisper
 import openai
 import json
 
+
+model = whisper.load_model("medium.en")
+result = model.transcribe("recording.mp3")
+print(result["text"])
+
+question=result['text']
+
 #this is the api key
 openai.api_key="your api key"
-question=input("Enter your question: ")
+# question=input("Enter your question: ")
 completion=openai.Completion.create(engine="text-davinci-003",prompt=question,max_tokens=1000)
 response=completion.choices[0]['text']
 
