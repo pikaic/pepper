553890af17796b75a55bba664e166df0112459ae
pikaic <pikaic2010@gmail.com>
2023-03-09 04:43:28 -0800

    commit

---
+++ chatgpt/speech_output.py
@@ -0,0 +1,60 @@
+#! /home/logan_18/anaconda3/envs/pepperenv/bin/python3
+# -*- encoding: UTF-8 -*-
+
+
+import pyaudio
+import wave
+import os
+import subprocess
+
+# Install lame mp3 encoder using pip
+# subprocess.run(["pip", "install", "pydub"])
+
+# Function to record audio
+def record_audio(path, filename, duration):
+    # Set the parameters for the audio stream
+    chunk = 1024
+    sample_format = pyaudio.paInt16
+    channels = 1
+    fs = 44100
+    
+    # Initialize the PyAudio object
+    p = pyaudio.PyAudio()
+    
+    # Open the audio stream
+    stream = p.open(format=sample_format,
+                    channels=channels,
+                    rate=fs,
+                    frames_per_buffer=chunk,
+                    input=True)
+    
+    frames = []
+    
+    # Record the audio for the specified duration
+    for i in range(int(fs / chunk * duration)):
+        data = stream.read(chunk)
+        frames.append(data)
+    
+    # Stop and close the audio stream
+    stream.stop_stream()
+    stream.close()
+    
+    # Terminate the PyAudio object
+    p.terminate()
+    
+    # Save the recorded audio as a WAV file
+    file_path = os.path.join(path, filename)
+    wf = wave.open(file_path, 'wb')
+    wf.setnchannels(channels)
+    wf.setsampwidth(p.get_sample_size(sample_format))
+    wf.setframerate(fs)
+    wf.writeframes(b''.join(frames))
+    wf.close()
+
+    # Convert the WAV file to MP3
+    # os.system(f"ffmpeg -i {filename} -acodec libmp3lame -aq 4 {filename[:-4]}.mp3")
+    
+# Example usage: Record 5 seconds of audio and save it as "recording.mp3"
+print("What do you want to know?")
+record_audio("./chatgpt", "recording.mp3", 7)
+print("Question recorded!!")
---
+++ chatgpt/tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+#! /home/logan_18/anaconda3/envs/pepperenv/bin/python3
+# -*- encoding: UTF-8 -*-
